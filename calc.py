8375ed81d284fbce61c3fc2c196da2291324855e
andersonalvesmartins78 <43176030+andersonalvesmartins78@users.noreply.github.com>
2018-09-11 16:55:18 -0300

    Add files via upload

--- calc.py
+++ calc.py
@@ -3,3 +3,19 @@ def soma(a,b):
   return float(a) + float(b)
  except:
   return None
+def sub(a,b):
+ try:
+  return float(a) - float(b)
+ except:
+  return None
+def mult(a,b):
+ try:
+  return float(a) * float(b)
+ except:
+  return None
+def div(a,b):
+ try:
+  return float(a) / float(b)
+ except:
+  return None
+
--- test_app.py
+++ test_app.py
@@ -1,5 +1,15 @@
 import pytest
-from calc import soma
-def test_app():
- assert soma(1,1)==2
- assert soma('1','1')==2
+from calc import soma,sub,mult,div
+@pytest.mark.parametrize('a,b,x', [(1,1,2),(1,2,3),(2,3,5)])
+def test_soma(a,b,x):
+ assert soma(a,b)==x
+@pytest.mark.parametrize('a,b,x', [(1,1,0),(1,2,-1),(2,3,-1)])
+def test_sub(a,b,x):
+ assert sub(a,b)==x
+@pytest.mark.parametrize('a,b,x', [(1,1,1),(1,2,2),(2,3,6)])
+def test_mult(a,b,x):
+ assert mult(a,b)==x
+@pytest.mark.parametrize('a,b,x', [(1,1,1),(1,2,0.5),(2,4,0.5)])
+def test_div(a,b,x):
+ assert div(a,b)==x
+
